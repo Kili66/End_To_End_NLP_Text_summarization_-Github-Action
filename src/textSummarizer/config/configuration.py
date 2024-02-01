1ab41a0ccceeefab5903d3ee715cd4588c97e0bd
Kili66 <mariamkilibech@gmail.com>
2024-02-01 22:39:14 +0300

    model evaluation done

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,34 +1,41 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directories
-from textSummarizer.entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig, ModelEvaluationConfig)
+from textSummarizer.entity import (DataIngestionConfig,
+                                   DataValidationConfig,
+                                   DataTransformationConfig,
+                                   ModelTrainerConfig,
+                                   ModelEvaluationConfig)
+
+
 class ConfigurationManager:
-    # access to constants variables
     def __init__(
-        self, 
-        config_file_path= CONFIG_FILE_PATH, 
-        params_filepath= PARAMS_FILE_PATH,
-        ):
-        self.config= read_yaml(config_file_path) #acces yaml file and read the content
-        self.params= read_yaml(params_filepath)
-        
-        create_directories([self.config.artifacts_root]) #configBox method, create the directory created on yaml: artifacts_root
-   
-   # ----------------------- Data Ingestion -------------  
-    def get_data_ingestion_config(self)->DataIngestionConfig:
-        config= self.config.data_ingestion
-        
-        create_directories([config.root_dir]) #data ingestion folder
-        
-        #return types
-        data_ingestion_config= DataIngestionConfig(
-            root_dir= config.root_dir,
-            source_URL= config.source_URL,
-            local_data_file= config.local_data_file,
-            unzip_dir= config.unzip_dir,
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+    
+
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
+        config = self.config.data_ingestion
+
+        create_directories([config.root_dir])
+
+        data_ingestion_config = DataIngestionConfig(
+            root_dir=config.root_dir,
+            source_URL=config.source_URL,
+            local_data_file=config.local_data_file,
+            unzip_dir=config.unzip_dir 
         )
+
         return data_ingestion_config
     
-    #----------------------- Data Validation-------------
+
+
     def get_data_validation_config(self) -> DataValidationConfig:
         config = self.config.data_validation
 
@@ -42,7 +49,7 @@ class ConfigurationManager:
 
         return data_validation_config
     
-    #####-----------Data Transformation------------
+
     def get_data_transformation_config(self) -> DataTransformationConfig:
         config = self.config.data_transformation
 
@@ -53,8 +60,11 @@ class ConfigurationManager:
             data_path=config.data_path,
             tokenizer_name = config.tokenizer_name
         )
+
         return data_transformation_config
-    ###------------------Model Trainer-----------
+    
+
+
     def get_model_trainer_config(self) -> ModelTrainerConfig:
         config = self.config.model_trainer
         params = self.params.TrainingArguments
@@ -78,6 +88,7 @@ class ConfigurationManager:
 
         return model_trainer_config
     
+
     def get_model_evaluation_config(self) -> ModelEvaluationConfig:
         config = self.config.model_evaluation
 
